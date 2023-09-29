87e3c9f6f8c2a19cafd057489f8695416f78f0a2
markabbe <markaabbe@gmail.com>
2023-09-29 15:57:16 -0400

    Progress on micro2

--- Micro2MarkAbbe/src/Micro2MarkAbbe.clj
+++ Micro2MarkAbbe/src/Micro2MarkAbbe.clj
@@ -1,22 +1,42 @@
 (ns Micro2MarkAbbe)
 
-(def randVar 10)
-
 (defn not-elimination [not-expression]
-  (cond
-    (not (and (vector? not-expression) (= (first not-expression) 'not) (= (count not-expression) 2)))
+  (if (and (= (first not-expression) not) (= (count not-expression) 2))
     (hash-set)
+    (if (and (vector? not-expression) (= (second not-expression) not) (= (first not-expression) not)
+           (= (count not-expression) 3))
+      (second not-expression)))
 
-    :else
-    (hash-set (second not-expression))
-    ))
 
-(def not_expr1 [:not :p])
-(def not_expr2 [:not :q])
-(def invalid_expr [:and :p :q])
+    ;:else
+    ;(hash-set (second not-expression))
+    ;)
+  )
+
+(def not_expr1 [not not :p])
+(def not_expr2 [not :q])
+(def invalid_expr [not :p :q])
+(def nonVecExpr (not 1))
 
 (defn -main
   [& args]
   (println (not-elimination not_expr1))
+  (println (count not_expr1))
+  (println (first not_expr1))
+  (println)
   (println (not-elimination not_expr2))
-  (println (not-elimination invalid_expr)))
+  (println (count not_expr2))
+  (println (first not_expr2))
+  (println)
+  (println (not-elimination invalid_expr))
+  (println (count invalid_expr))
+  (println (first invalid_expr))
+  (println)
+  (println (not-elimination '(not 1)))
+  (println)
+  (println (vector? []))
+  (println (vector? ()))
+  (println)
+  (if   ( = 2 2 )
+    (println "Equal")
+    (println "Not Equal")))
