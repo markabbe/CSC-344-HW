43c5fb74f2c71d5d3d6f8af05128003b8a12bf8a
markabbe <markaabbe@gmail.com>
2023-09-29 21:29:12 -0400

    Finished Micro2... Needed to be using lists rather than vectors!!

--- Micro2MarkAbbe/src/Micro2MarkAbbe.clj
+++ Micro2MarkAbbe/src/Micro2MarkAbbe.clj
@@ -1,42 +1,22 @@
 (ns Micro2MarkAbbe)
 
-(defn not-elimination [not-expression]
-  (if (and (= (first not-expression) not) (= (count not-expression) 2))
-    (hash-set)
-    (if (and (vector? not-expression) (= (second not-expression) not) (= (first not-expression) not)
-           (= (count not-expression) 3))
-      (second not-expression)))
-
-
-    ;:else
-    ;(hash-set (second not-expression))
-    ;)
+(defn not-elimination [not-prop]
+  (if (and (list? not-prop)
+           (= (first not-prop) 'not)
+           (list? (second not-prop))
+           (= (first (second not-prop)) 'not)) ;;End of conditions
+    #{(second (second not-prop))} ;;Then statement
+    #{}) ;;Else
   )
 
+;;Probably wont need (not using vectors)
 (def not_expr1 [not not :p])
 (def not_expr2 [not :q])
-(def invalid_expr [not :p :q])
-(def nonVecExpr (not 1))
 
 (defn -main
   [& args]
-  (println (not-elimination not_expr1))
-  (println (count not_expr1))
-  (println (first not_expr1))
-  (println)
-  (println (not-elimination not_expr2))
-  (println (count not_expr2))
-  (println (first not_expr2))
-  (println)
-  (println (not-elimination invalid_expr))
-  (println (count invalid_expr))
-  (println (first invalid_expr))
-  (println)
-  (println (not-elimination '(not 1)))
-  (println)
-  (println (vector? []))
-  (println (vector? ()))
-  (println)
-  (if   ( = 2 2 )
-    (println "Equal")
-    (println "Not Equal")))
+  (println (not-elimination '(not x))) ;;Output: #{}
+  (println (not-elimination '(not (not a)))) ;;Output: #{a}
+  (println (not-elimination '(not (not (and a b))))) ;;Output: #{(and a b)}
+  (println (not-elimination '(not (not (not (not c)))))) ;;Output: #{(not (not c))}
+  )
