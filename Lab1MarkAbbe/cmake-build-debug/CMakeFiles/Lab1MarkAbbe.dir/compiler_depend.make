1ba8cae52604b82e140bfa2c63dd60dcb7f684b0
markabbe <markaabbe@gmail.com>
2023-10-02 00:35:27 -0400

    Turing machine working

--- Lab1MarkAbbe/cmake-build-debug/CMakeFiles/Lab1MarkAbbe.dir/compiler_depend.make
+++ Lab1MarkAbbe/cmake-build-debug/CMakeFiles/Lab1MarkAbbe.dir/compiler_depend.make
@@ -24,25 +24,14 @@ CMakeFiles/Lab1MarkAbbe.dir/main.c.obj: C:/Users/marka/Documents/CSC-344-HW/Lab1
   C:/Program\ Files/JetBrains/CLion\ 2023.2.1/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/mm_malloc.h \
   C:/Program\ Files/JetBrains/CLion\ 2023.2.1/bin/mingw/x86_64-w64-mingw32/include/errno.h \
   C:/Program\ Files/JetBrains/CLion\ 2023.2.1/bin/mingw/x86_64-w64-mingw32/include/string.h \
-  C:/Program\ Files/JetBrains/CLion\ 2023.2.1/bin/mingw/x86_64-w64-mingw32/include/sec_api/string_s.h \
-  C:/Program\ Files/JetBrains/CLion\ 2023.2.1/bin/mingw/x86_64-w64-mingw32/include/unistd.h \
-  C:/Program\ Files/JetBrains/CLion\ 2023.2.1/bin/mingw/x86_64-w64-mingw32/include/io.h \
-  C:/Program\ Files/JetBrains/CLion\ 2023.2.1/bin/mingw/x86_64-w64-mingw32/include/process.h \
-  C:/Program\ Files/JetBrains/CLion\ 2023.2.1/bin/mingw/x86_64-w64-mingw32/include/corecrt_startup.h \
-  C:/Program\ Files/JetBrains/CLion\ 2023.2.1/bin/mingw/x86_64-w64-mingw32/include/sys/types.h \
-  C:/Program\ Files/JetBrains/CLion\ 2023.2.1/bin/mingw/x86_64-w64-mingw32/include/getopt.h \
-  C:/Program\ Files/JetBrains/CLion\ 2023.2.1/bin/mingw/x86_64-w64-mingw32/include/pthread_unistd.h
+  C:/Program\ Files/JetBrains/CLion\ 2023.2.1/bin/mingw/x86_64-w64-mingw32/include/sec_api/string_s.h
 
-
-C:/Program\ Files/JetBrains/CLion\ 2023.2.1/bin/mingw/x86_64-w64-mingw32/include/io.h:
 
 C:/Users/marka/Documents/CSC-344-HW/Lab1MarkAbbe/main.c:
 
 C:/Program\ Files/JetBrains/CLion\ 2023.2.1/bin/mingw/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h:
 
 C:/Program\ Files/JetBrains/CLion\ 2023.2.1/bin/mingw/x86_64-w64-mingw32/include/_mingw_secapi.h:
-
-C:/Program\ Files/JetBrains/CLion\ 2023.2.1/bin/mingw/x86_64-w64-mingw32/include/process.h:
 
 C:/Program\ Files/JetBrains/CLion\ 2023.2.1/bin/mingw/x86_64-w64-mingw32/include/_mingw.h:
 
@@ -57,8 +46,6 @@ C:/Program\ Files/JetBrains/CLion\ 2023.2.1/bin/mingw/x86_64-w64-mingw32/include
 C:/Program\ Files/JetBrains/CLion\ 2023.2.1/bin/mingw/x86_64-w64-mingw32/include/string.h:
 
 C:/Program\ Files/JetBrains/CLion\ 2023.2.1/bin/mingw/x86_64-w64-mingw32/include/_mingw_mac.h:
-
-C:/Program\ Files/JetBrains/CLion\ 2023.2.1/bin/mingw/x86_64-w64-mingw32/include/corecrt_startup.h:
 
 C:/Program\ Files/JetBrains/CLion\ 2023.2.1/bin/mingw/x86_64-w64-mingw32/include/corecrt_wstdlib.h:
 
@@ -87,11 +74,3 @@ C:/Program\ Files/JetBrains/CLion\ 2023.2.1/bin/mingw/lib/gcc/x86_64-w64-mingw32
 C:/Program\ Files/JetBrains/CLion\ 2023.2.1/bin/mingw/x86_64-w64-mingw32/include/errno.h:
 
 C:/Program\ Files/JetBrains/CLion\ 2023.2.1/bin/mingw/x86_64-w64-mingw32/include/sec_api/string_s.h:
-
-C:/Program\ Files/JetBrains/CLion\ 2023.2.1/bin/mingw/x86_64-w64-mingw32/include/unistd.h:
-
-C:/Program\ Files/JetBrains/CLion\ 2023.2.1/bin/mingw/x86_64-w64-mingw32/include/sys/types.h:
-
-C:/Program\ Files/JetBrains/CLion\ 2023.2.1/bin/mingw/x86_64-w64-mingw32/include/getopt.h:
-
-C:/Program\ Files/JetBrains/CLion\ 2023.2.1/bin/mingw/x86_64-w64-mingw32/include/pthread_unistd.h:
--- Lab1MarkAbbe/main.c
+++ Lab1MarkAbbe/main.c
@@ -1,215 +1,132 @@
-//Mark Abbe
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <limits.h>
-#include <unistd.h>
 
-#define TAPE_CELL_BLANK 'B'
-#define TAPE_CELL_BEGIN 'A'
-#define TAPE_CELL_SIZE 100
-
-struct Cell {
+typedef struct Cell {
     char value;
-    struct Cell* prev;
     struct Cell* next;
-};
+    struct Cell* previous;
+} Cell;
 
-struct Instruction {
-    int currentState;
-    char readVal;
+typedef struct {
     char writeVal;
     char moveDirection;
     int newState;
-};
+    int isSet; // 1 if this instruction is set, 0 otherwise
+} Instruction;
 
-struct TuringMachine {
-    struct Cell* tape;
-    int numStates;
-    int startState;
-    int endState;
-    struct Instruction* instructions;
-};
-
-// Function to initialize the Turing machine
-void initializeTuringMachine(struct TuringMachine* tm, const char* filename) {
-    // Debug: Print the filename
-    printf("%s\n\n", filename);
-
-    FILE* file = fopen(filename, "r");
-    if (file == NULL) {
-        perror("Error opening file");
-        exit(1);
-    }
-
-    char initialTape[TAPE_CELL_SIZE];
-    fscanf(file, "%s", initialTape);
-
-    // Read number of states, start state, and end state
-    fscanf(file, "%d", &tm->numStates);
-    fscanf(file, "%d", &tm->startState);
-    fscanf(file, "%d", &tm->endState);
-
-    // Read and parse instructions
-    int numInstructions;
-    fscanf(file, "%d", &numInstructions);
-    tm->instructions = (struct Instruction*)malloc(numInstructions * sizeof(struct Instruction));
-    for (int i = 0; i < numInstructions; i++) {
-        char buf[100];
-        fscanf(file, "%s", buf);
-        sscanf(buf, "(%d,%c)->(%c,%c,%d)",
-               &tm->instructions[i].currentState,
-               &tm->instructions[i].readVal,
-               &tm->instructions[i].writeVal,
-               &tm->instructions[i].moveDirection,
-               &tm->instructions[i].newState);
-    }
-
-    fclose(file);
-
-    tm->tape = (struct Cell*)malloc(sizeof(struct Cell));
-    tm->tape->value = TAPE_CELL_BEGIN;
-    tm->tape->prev = NULL;
-    tm->tape->next = NULL;
-
-    // Add input to the tape
-    struct Cell* current = tm->tape;
-    for (int i = 0; initialTape[i] != '\0'; i++) {
-        struct Cell* newCell = (struct Cell*)malloc(sizeof(struct Cell));
-        newCell->value = initialTape[i];
-        newCell->prev = current;
-        newCell->next = NULL;
-        current->next = newCell;
-        current = newCell;
-    }
-}
-
-// Function to execute one step of the Turing machine
-void executeStep(struct TuringMachine* tm) {
-    char currentValue = tm->tape->value;
-    int i;
-
-    for (i = 0; i < tm->numStates; i++) {
-        if (tm->instructions[i].currentState == tm->startState && tm->instructions[i].readVal == currentValue) {
-            // Execute the instruction
-            tm->tape->value = tm->instructions[i].writeVal;
-
-            if (tm->instructions[i].moveDirection == 'L') {
-                if (tm->tape->prev == NULL) {
-                    // Create new cell on the left
-                    struct Cell* newCell = (struct Cell*)malloc(sizeof(struct Cell));
-                    newCell->value = TAPE_CELL_BLANK;
-                    newCell->prev = NULL;
-                    newCell->next = tm->tape;
-                    tm->tape->prev = newCell;
-                    tm->tape = newCell;
-                }
-                else {
-                    tm->tape = tm->tape->prev;
-                }
-            }
-            else if (tm->instructions[i].moveDirection == 'R') {
-                if (tm->tape->next == NULL) {
-                    // Create new cell on the right
-                    struct Cell* newCell = (struct Cell*)malloc(sizeof(struct Cell));
-                    newCell->value = TAPE_CELL_BLANK;
-                    newCell->prev = tm->tape;
-                    newCell->next = NULL;
-                    tm->tape->next = newCell;
-                }
-                else {
-                    tm->tape = tm->tape->next;
-                }
-            }
-
-            tm->startState = tm->instructions[i].newState;
-            return; // Exit the function after executing one instruction
-        }
-    }
-
-    // If no instruction is found then halt
-    printf("Halt.\n");
-    exit(0);
+Cell* createCell(char value, Cell* prev, Cell* next) {
+    Cell* cell = (Cell*)malloc(sizeof(Cell));
+    cell->value = value;
+    cell->previous = prev;
+    cell->next = next;
+    return cell;
 }
 
 int main() {
-    struct TuringMachine tm;
-    char input[256];
-    char filename[256];
+    char filename[100];
+    printf("Enter the filename for the TM input: ");
+    scanf("%s", filename);
 
-    printf("Enter the filename (make sure the file is located in the root directory of the project): ");
-
-    if (fgets(input, sizeof(input), stdin) == NULL) {
-        fprintf(stderr, "Error reading input filename\n");
+    FILE* file = fopen(filename, "r");
+    if (!file) {
+        printf("Error opening file.\n");
         return 1;
     }
 
-    input[strcspn(input, "\n")] = '\0';
+    char initialTape[100];
+    int numStates, startState, endState;
+    fscanf(file, "%s\n%d\n%d\n%d\n", initialTape, &numStates, &startState, &endState);
 
-    if (getcwd(filename, sizeof(filename)) == NULL) {
-        perror("getcwd");
-        return 1;
+    Instruction instructions[numStates][128];
+    memset(instructions, 0, sizeof(instructions)); // Initialize with zeros
+
+    while (!feof(file)) {
+        int currentState;
+        char readVal, writeVal, moveDirection;
+        int newState;
+        fscanf(file, "(%d,%c)->(%c,%c,%d)\n", &currentState, &readVal, &writeVal, &moveDirection, &newState);
+        instructions[currentState][(int)readVal] = (Instruction){writeVal, moveDirection, newState, 1};
     }
+    fclose(file);
 
-    // Construct the full file path by appending ../ to the user-provided filename
-    strncat(filename, "/../", sizeof(filename) - strlen(filename) - 1);
-    strncat(filename, input, sizeof(filename) - strlen(filename) - 1);
-
-    initializeTuringMachine(&tm, filename);
-
-    struct Cell* head = tm.tape;
-
-    // Print initial tape contents
-    printf("Initial tape contents: ");
-    struct Cell* current = tm.tape;
-    while (current != NULL) {
-        printf("%c", current->value);
-        current = current->next;
-    }
-    printf("\n");
-
-    while (1) {
-        // Print current state and tape
-        printf("State %d\n", tm.startState);
-        current = tm.tape;
-        while (current != NULL) {
-            if (current == head) {
-                printf("^");
+    // Debug: Print instructions
+    printf("Instructions:\n");
+    for (int i = 0; i < numStates; i++) {
+        for (int j = 0; j < 128; j++) {
+            if (instructions[i][j].isSet) {
+                printf("(%d,%c)->(%c,%c,%d)\n", i, (char)j, instructions[i][j].writeVal, instructions[i][j].moveDirection, instructions[i][j].newState);
             }
-            else {
-                printf(" ");
-            }
-            printf("%c", current->value);
-            current = current->next;
-        }
-        printf("\n");
-
-        // Execute a step
-        executeStep(&tm);
-
-        // Check if reached the end state
-        if (tm.startState == tm.endState) {
-            break;
         }
     }
 
-    // Not sure why this doesnt work?
-    printf("\nFinal tape contents: ");
-    current = tm.tape;
-    while (current != NULL && current->value != TAPE_CELL_BLANK) {
-        printf("%c", current->value);
+    // Create the initial tape
+    Cell* tape = createCell('A', NULL, NULL);
+    Cell* current = tape;
+    for (int i = 0; initialTape[i] != '\0'; i++) {
+        current->next = createCell(initialTape[i], current, NULL);
         current = current->next;
+    }
+
+    // Turing Machine Execution
+    Cell* currentCell = tape; // start at the beginning
+    int currentState = startState;
+
+    printf("Starting Turing machine...\n");
+    while (currentState != endState) {
+        printf("Current state: %d, Current value: %c\n", currentState, currentCell->value);
+        Instruction instr = instructions[currentState][(int)currentCell->value];
+
+        if (!instr.isSet) {
+            printf("Error: No instruction found for (State %d, Value %c).\n", currentState, currentCell->value);
+            return 1; // Exit with error code
+        }
+
+        printf("Fetched transition: (%d, %c) -> (%c, %c, %d)\n", currentState, currentCell->value, instr.writeVal, instr.moveDirection, instr.newState);
+        printf("Write %c, Move %c, New State %d\n------------\n", instr.writeVal, instr.moveDirection, instr.newState);
+
+        if (instr.writeVal == 'B' && currentCell->value == 'B' && !currentCell->next) {
+            currentCell->next = createCell('B', currentCell, NULL);
+            currentState = endState; // Transition to the final state
+            continue; // Skip other logic and move to the next iteration
+        }
+
+        currentCell->value = instr.writeVal;
+        if (instr.moveDirection == 'R') {
+            if (!currentCell->next) {
+                currentCell->next = createCell('B', currentCell, NULL);
+            }
+            currentCell = currentCell->next;
+        } else if (instr.moveDirection == 'L') {
+            if (currentCell->previous) {
+                currentCell = currentCell->previous;
+            } else {
+                printf("Error: Trying to move left at the beginning of the tape.\n");
+                return 1;
+            }
+        } else {
+            printf("Error: Invalid move direction.\n");
+            return 1;
+        }
+
+        currentState = instr.newState;
+    }
+
+    // Print final tape contents
+    printf("Final tape contents: ");
+    currentCell = tape;
+    while (currentCell) {
+        printf("%c", currentCell->value);
+        currentCell = currentCell->next;
     }
     printf("\n");
 
-    // Free the memory used by the tape and instructions
-    current = tm.tape;
-    while (current != NULL) {
-        struct Cell* temp = current;
-        current = current->next;
+    // Clean up (deallocate memory)
+    while (tape) {
+        Cell* temp = tape;
+        tape = tape->next;
         free(temp);
     }
-    free(tm.instructions);
 
     return 0;
 }
