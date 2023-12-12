035c3e174e337ec8a406b7e4aa9d74446e59d1f7
markabbe <markaabbe@gmail.com>
2023-12-12 16:45:28 -0500

    test

--- Micro5MarkAbbe/main.py
+++ Micro5MarkAbbe/main.py
@@ -4,12 +4,14 @@ import shutil
 import tarfile
 from collections import defaultdict
 
+# Clear the __pycache__ directory to avoid any caching issues
 def clear_pycache(script_path):
     pycache_path = os.path.join(script_path, '__pycache__')
     if os.path.exists(pycache_path) and os.path.isdir(pycache_path):
         shutil.rmtree(pycache_path)
         print(f"Cleared pycache at {pycache_path}")
 
+# Extract identifiers from the file content
 def extract_identifiers(file_content):
     identifiers = set()
     lines = file_content.split('\n')
@@ -19,6 +21,7 @@ def extract_identifiers(file_content):
         identifiers.update(possible_identifiers)
     return sorted(identifiers)
 
+# Create HTML summary for each assignment
 def create_html_summary(directory, summary_name, file_name=None):
     all_identifiers = set()
     html_content = f"<html><head><title>Summary of {os.path.basename(directory)}</title></head><body><h1>Summary of {os.path.basename(directory)}</h1>"
@@ -47,35 +50,33 @@ def create_html_summary(directory, summary_name, file_name=None):
         file.write(html_content)
         print(f"Created summary at {summary_file_path}")
 
+# Create index HTML with links to all summaries
 def create_index_html(parent_directory, assignments):
-    index_content = "<html><head><title>Index of Summaries</title></head><body>"
-    index_content += "<h1>Index of Assignment Summaries</h1><ul>"
-
+    index_content = "<html><head><title>Index of Summaries</title></head><body><h1>Index of Assignment Summaries</h1><ul>"
     for assignment, summary_name in assignments.items():
         summary_path = os.path.join(assignment, summary_name)
         index_content += f'<li><a href="{summary_path}">{summary_name}</a></li>'
-
     index_content += "</ul></body></html>"
-
     index_file_path = os.path.join(parent_directory, 'index.html')
     with open(index_file_path, 'w', encoding='utf-8') as file:
         file.write(index_content)
         print(f"Created index at {index_file_path}")
 
+# Create a tar.gz archive with all the sources and HTML files
 def create_tar_gz(parent_directory, assignments, output_filename='assignment_sources.tar.gz'):
     with tarfile.open(output_filename, "w:gz") as tar:
         for assignment, summary_name in assignments.items():
             assignment_path = os.path.join(parent_directory, assignment)
             for root, dirs, files in os.walk(assignment_path):
                 for file in files:
-                    if file.endswith(('.c', '.clj', '.ml', '.lp', '.py', summary_name)):
+                    if file.endswith(('.c', '.clj', '.ml', '.lp', '.py')) or file == summary_name:
                         file_path = os.path.join(root, file)
                         tar.add(file_path, arcname=os.path.relpath(file_path, parent_directory))
-
         index_path = os.path.join(parent_directory, 'index.html')
-        tar.add(index_path, arcname=os.path.relpath(index_path, parent_directory))
+        tar.add(index_path, arcname='index.html')
         print(f"Created archive at {output_filename}")
 
+# Process each assignment directory and create summaries
 def process_directories(parent_directory):
     assignments = {
         'Lab1MarkAbbe': 'summary_a1.html',
@@ -88,16 +89,14 @@ def process_directories(parent_directory):
         path = os.path.join(parent_directory, assignment)
         create_html_summary(path, summary_name)
         print(f"Processed {assignment}")
-
     create_index_html(parent_directory, assignments)
 
+# Assuming the script is located in Micro5MarkAbbe directory
 clear_pycache(os.getcwd())
-
 script_location = os.getcwd()
 parent_directory = os.path.abspath(os.path.join(script_location, '..'))
 print(f"Script started. Parent directory: {parent_directory}")
 process_directories(parent_directory)
-
 create_tar_gz(parent_directory, {
     'Lab1MarkAbbe': 'summary_a1.html',
     'Micro2MarkAbbe': 'summary_a2.html',
