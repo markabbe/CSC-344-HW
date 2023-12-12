b0ced1be12558c902927b117aee6e14eb34a4a51
markabbe <markaabbe@gmail.com>
2023-12-11 21:08:07 -0500

    Test

--- Micro5MarkAbbe/main.py
+++ Micro5MarkAbbe/main.py
@@ -1,28 +1,46 @@
 import os
 import subprocess
-import sys
+import re
+from collections import defaultdict
 
-def count_lines_in_directory(directory):
-    # Check if the directory exists
+
+def extract_identifiers(file_path):
+    identifiers = set()
+    with open(file_path, 'r') as file:
+        for line in file:
+            # Simple filter for comments and string literals
+            # This is very basic and might not cover all cases
+            line = re.sub(r'".*?"|\'.*?\'|#.*|//.*|/\*.*?\*/', '', line)
+            # Extracting words that might be identifiers
+            possible_identifiers = re.findall(r'\b[_a-zA-Z][_a-zA-Z0-9]*\b', line)
+            identifiers.update(possible_identifiers)
+    return list(identifiers)
+
+
+def create_html_summary(directory):
     if not os.path.isdir(directory):
         print(f"Directory '{directory}' not found.")
         return
 
-    # Iterate over files in the directory
+    summary = defaultdict(list)
+
     for filename in os.listdir(directory):
         file_path = os.path.join(directory, filename)
 
-        # Check if it's a file
-        if os.path.isfile(file_path):
-            # Use wc command to count lines
+        if os.path.isfile(file_path) and file_path.endswith(('.c', '.clj', '.ml', '.lp', '.py')):
             result = subprocess.run(['wc', '-l', file_path], capture_output=True, text=True)
-
-            # Extract the line count from the command output
             line_count = result.stdout.split()[0]
-            print(f"{filename}: {line_count} lines")
 
-# Check if a directory argument is provided
-if len(sys.argv) != 2:
-    print("Usage: python3 main.py <directory>")
-else:
-    count_lines_in_directory(sys.argv[1])
+            identifiers = extract_identifiers(file_path)
+            summary[filename] = {'lines': line_count, 'identifiers': identifiers}
+
+    html_content = "<html><body><h1>Summary of " + os.path.basename(directory) + "</h1>"
+    for file, details in summary.items():
+        html_content += f"<h2>{file} ({details['lines']} lines)</h2><ul>"
+        for identifier in sorted(details['identifiers']):
+            html_content += f"<li>{identifier}</li>"
+        html_content += "</ul>"
+    html_content += "</body></html>"
+
+    with open(os.path.join(directory, 'summary.html'), 'w') as file:
+        file.write(html_content)
