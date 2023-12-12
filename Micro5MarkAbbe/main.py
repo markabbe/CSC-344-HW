d779f0e88a0c6510e0c3449038db64c9bd9ff8c8
markabbe <markaabbe@gmail.com>
2023-12-12 02:10:43 -0500

    test

--- Micro5MarkAbbe/main.py
+++ Micro5MarkAbbe/main.py
@@ -4,10 +4,13 @@ import re
 import shutil
 from collections import defaultdict
 
+
 def clear_pycache(script_path):
     pycache_path = os.path.join(script_path, '__pycache__')
     if os.path.exists(pycache_path) and os.path.isdir(pycache_path):
         shutil.rmtree(pycache_path)
+        print(f"Cleared pycache at {pycache_path}")
+
 
 def extract_identifiers(file_path):
     identifiers = set()
@@ -16,7 +19,9 @@ def extract_identifiers(file_path):
             line = re.sub(r'".*?"|\'.*?\'|#.*|//.*|/\*.*?\*/', '', line)
             possible_identifiers = re.findall(r'\b[_a-zA-Z][_a-zA-Z0-9]*\b', line)
             identifiers.update(possible_identifiers)
+    print(f"Extracted identifiers from {file_path}")
     return list(identifiers)
+
 
 def create_html_summary(directory, summary_name):
     summary = defaultdict(list)
@@ -27,6 +32,8 @@ def create_html_summary(directory, summary_name):
             line_count = result.stdout.split()[0]
             identifiers = extract_identifiers(file_path)
             summary[filename] = {'lines': line_count, 'identifiers': identifiers}
+            print(f"Processed file {file_path}")
+
     html_content = "<html><body><h1>Summary of " + os.path.basename(directory) + "</h1>"
     for file, details in summary.items():
         html_content += f"<h2>{file} ({details['lines']} lines)</h2><ul>"
@@ -34,28 +41,31 @@ def create_html_summary(directory, summary_name):
             html_content += f"<li>{identifier}</li>"
         html_content += "</ul>"
     html_content += "</body></html>"
+
     summary_file_path = os.path.join(directory, summary_name)
     with open(summary_file_path, 'w') as file:
         file.write(html_content)
+        print(f"Created summary at {summary_file_path}")
+
 
 def process_directories(parent_directory):
     assignments = {
         'Lab1MarkAbbe': 'summary_a1.html',
         'Micro2MarkAbbe': 'summary_a2.html',
         'Lab3MarkAbbe': 'summary_a3.html',
-        'Lab4MarkAbbe.lp': 'summary_a4.html',  # This is a file, handle separately
         'Micro5MarkAbbe': 'summary_a5.html'
     }
     for assignment, summary_name in assignments.items():
         dir_path = os.path.join(parent_directory, assignment)
-        if os.path.isdir(dir_path):
-            create_html_summary(dir_path, summary_name)
-        elif os.path.isfile(dir_path):  # If it's a file and not a directory
-            create_html_summary(parent_directory, summary_name)
+        if os.path.isdir(dir_path) or os.path.isfile(dir_path):
+            create_html_summary(dir_path if os.path.isdir(dir_path) else parent_directory, summary_name)
+            print(f"Processed directory {dir_path}")
+
 
 # Clear the __pycache__ directory to avoid any caching issues
 clear_pycache(os.getcwd())
 
 # Assuming the script is located in Micro5MarkAbbe, so we traverse back up two levels to reach the CSC-344-HW directory
 parent_directory = os.path.abspath(os.path.join(os.getcwd(), '../..'))
+print(f"Parent directory: {parent_directory}")
 process_directories(parent_directory)
