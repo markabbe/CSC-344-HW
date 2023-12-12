5df71547c6764af13a732b8ab05b8aa7970eb74c
markabbe <markaabbe@gmail.com>
2023-12-12 02:51:00 -0500

    test

--- Micro5MarkAbbe/main.py
+++ Micro5MarkAbbe/main.py
@@ -24,32 +24,41 @@ def extract_identifiers(file_path):
 
 
 def create_html_summary(directory, summary_name, file_name=None):
-    summary = defaultdict(list)
-    file_list = [file_name] if file_name else os.listdir(directory)
-
+    # Correctly initialize the list of identifiers
+    all_identifiers = set()
     html_content = f"<html><head><title>Summary of {os.path.basename(directory)}</title></head><body><h1>Summary of {os.path.basename(directory)}</h1>"
+
+    # Get the correct list of files
+    file_list = [file_name] if file_name else [f for f in os.listdir(directory) if
+                                               os.path.isfile(os.path.join(directory, f))]
 
     for filename in file_list:
         file_path = os.path.join(directory, filename) if not file_name else filename
-        if os.path.isfile(file_path) and file_path.endswith(('.c', '.clj', '.ml', '.lp', '.py')):
-            # Count lines without using wc
-            with open(file_path, 'r', encoding='utf-8', errors='ignore') as file:
-                content = file.readlines()
-                line_count = len(content)
-                identifiers = extract_identifiers(''.join(content))
-                summary[filename] = {'lines': line_count, 'identifiers': sorted(identifiers)}
-            html_content += f"<h2><a href=\"{filename}\">{filename}</a> ({line_count} lines)</h2>"
-            html_content += "<ul>"
-            for identifier in summary[filename]['identifiers']:
-                html_content += f"<li>{identifier}</li>"
-            html_content += "</ul>"
+        if file_path.endswith(('.c', '.clj', '.ml', '.lp', '.py')):
+            # Open the file and read its content
+            try:
+                with open(file_path, 'r', encoding='utf-8', errors='ignore') as file:
+                    content = file.read()
+                    line_count = content.count('\n') + 1
+                    identifiers = extract_identifiers(content)
+                    all_identifiers.update(identifiers)  # Update the set of all identifiers
+                    # Add file info to the HTML content
+                    html_content += f"<h2><a href=\"{filename}\">{filename}</a> ({line_count} lines)</h2>\n"
+            except OSError as e:
+                print(f"Error processing file {file_path}: {e}")
 
-    html_content += "</body></html>"
+    # Add identifiers to the HTML content
+    html_content += "<h2>Identifiers:</h2><ul>\n"
+    for identifier in sorted(all_identifiers):
+        html_content += f"<li>{identifier}</li>\n"
+    html_content += "</ul></body></html>"
 
+    # Write the HTML content to the summary file
     summary_file_path = os.path.join(directory, summary_name)
     with open(summary_file_path, 'w', encoding='utf-8') as file:
         file.write(html_content)
         print(f"Created summary at {summary_file_path}")
+
 
 def process_directories(parent_directory):
     assignments = {
